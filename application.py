c9df4c0ea4a5ded941765e474d51d2ec7f0b4ae2
arc7722 <sreinolds@gmail.com>
2019-02-06 11:55:52 +0000

    mail server changes

--- application.py
+++ application.py
@@ -16,6 +16,7 @@ import time
 
 app = Flask(__name__)
 
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_SERVER'] = os.environ.get("MAIL_SERVER")
 app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
